bc36fbd08077ebe83a3c07ed02ee5e2054f23c26
Muhammad Arslan <m.arslanijaz9@gmail.com>
2023-08-09 13:04:36 +0500

    Fix linting errors in vanna types.py

--- src/vanna/types.py
+++ src/vanna/types.py
@@ -2,14 +2,17 @@ from __future__ import annotations
 from typing import List, Dict, Union
 from dataclasses import dataclass
 
+
 @dataclass
 class Status:
     success: bool
     message: str
 
+
 @dataclass
 class QuestionList:
     questions: List[FullQuestionDocument]
+
 
 @dataclass
 class FullQuestionDocument:
@@ -19,11 +22,13 @@ class FullQuestionDocument:
     data: DataResult | None
     plotly: PlotlyResult | None
 
+
 @dataclass
 class QuestionSQLPair:
     question: str
     sql: str
     tag: Union[str, None]
+
 
 @dataclass
 class Organization:
@@ -31,26 +36,32 @@ class Organization:
     user: str | None
     connection: Connection | None
 
+
 @dataclass
 class OrganizationList:
     organizations: List[str]
+
 
 @dataclass
 class QuestionStringList:
     questions: List[str]
 
+
 @dataclass
 class Visibility:
     visibility: bool
+
 
 @dataclass
 class UserEmail:
     email: str
 
+
 @dataclass
 class NewOrganization:
     org_name: str
     db_type: str
+
 
 @dataclass
 class NewOrganizationMember:
@@ -58,22 +69,27 @@ class NewOrganizationMember:
     email: str
     is_admin: bool
 
+
 @dataclass
 class UserOTP:
     email: str
     otp: str
 
+
 @dataclass
 class ApiKey:
     key: str
+
 
 @dataclass
 class QuestionId:
     id: str
 
+
 @dataclass
 class Question:
     question: str
+
 
 @dataclass
 class QuestionCategory:
@@ -87,26 +103,31 @@ class QuestionCategory:
     FLAGGED_FOR_REVIEW = "Flagged for Review"
     REVIEWED_AND_APPROVED = "Reviewed and Approved"
     REVIEWED_AND_REJECTED = "Reviewed and Rejected"
-    REVIEWED_AND_UPDATED = "Reviewed and Updated" 
+    REVIEWED_AND_UPDATED = "Reviewed and Updated"
+
 
 @dataclass
 class AccuracyStats:
     num_questions: int
     data: Dict[str, int]
 
+
 @dataclass
 class Followup:
     followup: str
+
 
 @dataclass
 class QuestionEmbedding:
     question: Question
     embedding: List[float]
 
+
 @dataclass
 class Connection:
-    #TODO: implement
+    # TODO: implement
     pass
+
 
 @dataclass
 class SQLAnswer:
@@ -115,9 +136,11 @@ class SQLAnswer:
     postfix: str
     sql: str
 
+
 @dataclass
 class Explanation:
     explanation: str
+
 
 @dataclass
 class DataResult:
@@ -127,14 +150,17 @@ class DataResult:
     error: str | None
     correction_attempts: int
 
+
 @dataclass
 class PlotlyResult:
     plotly_code: str
+
 
 @dataclass
 class WarehouseDefinition:
     name: str
     tables: List[TableDefinition]
+
 
 @dataclass
 class TableDefinition:
@@ -142,6 +168,7 @@ class TableDefinition:
     table_name: str
     ddl: str | None
     columns: List[ColumnDefinition]
+
 
 @dataclass
 class ColumnDefinition:
@@ -152,14 +179,17 @@ class ColumnDefinition:
     foreign_key_table: str
     foreign_key_column: str
 
+
 @dataclass
 class Diagram:
     raw: str
     mermaid_code: str
 
+
 @dataclass
 class StringData:
     data: str
+
 
 @dataclass
 class DataFrameJSON:
