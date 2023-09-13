4a1268b806202cccc497ca0a68de1cf529788217
Zain Hoda <7146154+zainhoda@users.noreply.github.com>
2023-09-13 14:36:49 -0400

    database connections

--- src/vanna/base/base.py
+++ src/vanna/base/base.py
@@ -1,3 +1,4 @@
+import json
 import os
 import sqlite3
 import traceback
@@ -12,6 +13,7 @@ import requests
 
 from ..exceptions import DependencyError, ImproperlyConfigured, ValidationError
 from ..types import TrainingPlan, TrainingPlanItem
+from ..utils import validate_config_path
 
 
 class VannaBase(ABC):
@@ -202,6 +204,195 @@ class VannaBase(ABC):
         self.run_sql = run_sql_sqlite
         self.run_sql_is_set = True
 
+    def connect_to_postgres(
+        self,
+        host: str = None,
+        dbname: str = None,
+        user: str = None,
+        password: str = None,
+        port: int = None,
+    ):
+        """
+        Connect to postgres using the psycopg2 connector. This is just a helper function to set [`vn.run_sql`][vanna.run_sql]
+        **Example:**
+        ```python
+        vn.connect_to_postgres(
+            host="myhost",
+            dbname="mydatabase",
+            user="myuser",
+            password="mypassword",
+            port=5432
+        )
+        ```
+        Args:
+            host (str): The postgres host.
+            dbname (str): The postgres database name.
+            user (str): The postgres user.
+            password (str): The postgres password.
+            port (int): The postgres Port.
+        """
+
+        try:
+            import psycopg2
+            import psycopg2.extras
+        except ImportError:
+            raise DependencyError(
+                "You need to install required dependencies to execute this method,"
+                " run command: \npip install vanna[postgres]"
+            )
+
+        if not host:
+            host = os.getenv("HOST")
+
+        if not host:
+            raise ImproperlyConfigured("Please set your postgres host")
+
+        if not dbname:
+            dbname = os.getenv("DATABASE")
+
+        if not dbname:
+            raise ImproperlyConfigured("Please set your postgres database")
+
+        if not user:
+            user = os.getenv("PG_USER")
+
+        if not user:
+            raise ImproperlyConfigured("Please set your postgres user")
+
+        if not password:
+            password = os.getenv("PASSWORD")
+
+        if not password:
+            raise ImproperlyConfigured("Please set your postgres password")
+
+        if not port:
+            port = os.getenv("PORT")
+
+        if not port:
+            raise ImproperlyConfigured("Please set your postgres port")
+
+        conn = None
+
+        try:
+            conn = psycopg2.connect(
+                host=host,
+                dbname=dbname,
+                user=user,
+                password=password,
+                port=port,
+            )
+        except psycopg2.Error as e:
+            raise ValidationError(e)
+
+        def run_sql_postgres(sql: str) -> Union[pd.DataFrame, None]:
+            if conn:
+                try:
+                    cs = conn.cursor()
+                    cs.execute(sql)
+                    results = cs.fetchall()
+
+                    # Create a pandas dataframe from the results
+                    df = pd.DataFrame(
+                        results, columns=[desc[0] for desc in cs.description]
+                    )
+                    return df
+
+                except psycopg2.Error as e:
+                    conn.rollback()
+                    raise ValidationError(e)
+
+        self.run_sql_is_set = True
+        self.run_sql = run_sql_postgres
+
+    def connect_to_bigquery(self, cred_file_path: str = None, project_id: str = None):
+        """
+        Connect to gcs using the bigquery connector. This is just a helper function to set [`vn.run_sql`][vanna.run_sql]
+        **Example:**
+        ```python
+        vn.connect_to_bigquery(
+            project_id="myprojectid",
+            cred_file_path="path/to/credentials.json",
+        )
+        ```
+        Args:
+            project_id (str): The gcs project id.
+            cred_file_path (str): The gcs credential file path
+        """
+
+        try:
+            from google.api_core.exceptions import GoogleAPIError
+            from google.cloud import bigquery
+            from google.oauth2 import service_account
+        except ImportError:
+            raise DependencyError(
+                "You need to install required dependencies to execute this method, run command:"
+                " \npip install vanna[bigquery]"
+            )
+
+        if not project_id:
+            project_id = os.getenv("PROJECT_ID")
+
+        if not project_id:
+            raise ImproperlyConfigured("Please set your Google Cloud Project ID.")
+
+        import sys
+
+        if "google.colab" in sys.modules:
+            try:
+                from google.colab import auth
+
+                auth.authenticate_user()
+            except Exception as e:
+                raise ImproperlyConfigured(e)
+        else:
+            print("Not using Google Colab.")
+
+        conn = None
+
+        try:
+            conn = bigquery.Client(project=project_id)
+        except:
+            print("Could not found any google cloud implicit credentials")
+
+        if cred_file_path:
+            # Validate file path and pemissions
+            validate_config_path(cred_file_path)
+        else:
+            if not conn:
+                raise ValidationError(
+                    "Pleae provide a service account credentials json file"
+                )
+
+        if not conn:
+            with open(cred_file_path, "r") as f:
+                credentials = service_account.Credentials.from_service_account_info(
+                    json.loads(f.read()),
+                    scopes=["https://www.googleapis.com/auth/cloud-platform"],
+                )
+
+            try:
+                conn = bigquery.Client(project=project_id, credentials=credentials)
+            except:
+                raise ImproperlyConfigured(
+                    "Could not connect to bigquery please correct credentials"
+                )
+
+        def run_sql_bigquery(sql: str) -> Union[pd.DataFrame, None]:
+            if conn:
+                try:
+                    job = conn.query(sql)
+                    df = job.result().to_dataframe()
+                    return df
+                except GoogleAPIError as error:
+                    errors = []
+                    for error in error.errors:
+                        errors.append(error["message"])
+                    raise errors
+            return None
+
+        self.run_sql_is_set = True
+        self.run_sql = run_sql_bigquery
+
     def run_sql(sql: str, **kwargs) -> pd.DataFrame:
         raise NotImplementedError(
             "You need to connect_to_snowflake or other database first."
@@ -324,10 +515,7 @@ class VannaBase(ABC):
         """
 
         if question and not sql:
-            example_question = "What is the average salary of employees?"
-            raise ValidationError(
-                f"Please also provide a SQL query \n Example Question:  {example_question}\n Answer: {ask(question=example_question)}"
-            )
+            raise ValidationError(f"Please also provide a SQL query")
 
         if documentation:
             print("Adding documentation....")
