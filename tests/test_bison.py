ebf92336cbaad3bef470e1c26a4da837af970f31
André Pedersen <andrped94@gmail.com>
2024-02-26 21:44:23 +0100

    Simplified bison test

--- tests/test_bison.py
+++ tests/test_bison.py
@@ -5,25 +5,15 @@ from vanna.palm.palm import Palm
 
 
 def test_bison_model():
-    # params to use with Bison
-    config = {
-        "temperature": 0.7,  # Temperature controls the degree of randomness in token selection.
-        "max_output_tokens": 256,  # Token limit determines the maximum amount of text output.
-        "top_p": 0.96,  # Tokens are selected from most probable to least until the sum of their probabilities equals the top_p value.
-        "top_k": 40,  # A top_k of 1 means the selected token is the most probable among all tokens.
-        "model": "chat-bison@001",  # Which model to use, e.g., Bison
-        "client": ChatModel("chat-bison@001"),
-    }
 
     class MyVanna(ChromaDB_VectorStore, Palm):
         def __init__(self, config=None):
             ChromaDB_VectorStore.__init__(self, config=config)
-            Palm.__init__(self, config=config)
+            Palm.__init__(self, client=ChatModel("chat-bison@001"), config=config)
 
-    vn = MyVanna(config=config)
+    vn = MyVanna()
 
     response = vn.generate_sql("How many chocolates are there?")
-
     print(f"Response from Model: {response.text}")
 
     return response
