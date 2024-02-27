5a23d61e3480b20161f7219aeeca5f6b9b4557cb
André Pedersen <andrped94@gmail.com>
2024-02-27 16:04:10 +0100

    Sets number of documents to retrieve from sql collection through config variable

--- src/vanna/chromadb/chromadb_vector.py
+++ src/vanna/chromadb/chromadb_vector.py
@@ -231,6 +231,7 @@ class ChromaDB_VectorStore(VannaBase):
         return ChromaDB_VectorStore._extract_documents(
             self.sql_collection.query(
                 query_texts=[question],
+                n_results=self.n_results,
             )
         )
 
