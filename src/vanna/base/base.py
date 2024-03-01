e1a1f02ab891df833d907652d371b76e5b83eed6
sathyapriyaa13 <147392056+sathyapriyaa13@users.noreply.github.com>
2024-03-01 20:39:03 +0530

    Update base.py

--- src/vanna/base/base.py
+++ src/vanna/base/base.py
@@ -722,7 +722,7 @@ class VannaBase(ABC):
 
         def run_sql_postgres(sql: str) -> Union[pd.DataFrame, None]:
                 try: 
-                  with self.connect_to_postgres() as conn:
+                  with self.connect_to_postgres(host='haroldwaste-db-demo-do-user-7032271-0.c.db.ondigitalocean.com', dbname='haroldwaste-db-demo-pool', user='doadmin', password='AVNS_EnFaUaOOP6-CwcUDyEv', port='25061') as conn:
                 
                     cs = conn.cursor()
                     cs.execute(sql)
