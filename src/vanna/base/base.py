7a3127495bd6b7404ad8d7488e2923a4e09b6979
史继坤 <shijikun@djcps.com>
2024-03-05 10:00:47 +0800

    add support for mysql

--- src/vanna/base/base.py
+++ src/vanna/base/base.py
@@ -796,6 +796,90 @@ class VannaBase(ABC):
         self.run_sql_is_set = True
         self.run_sql = run_sql_postgres
 
+
+    def connect_to_mysql(
+            self,
+            host: str = None,
+            dbname: str = None,
+            user: str = None,
+            password: str = None,
+            port: int = None,
+    ):
+
+        try:
+            import pymysql.cursors
+        except ImportError:
+            raise DependencyError(
+                "You need to install required dependencies to execute this method,"
+                " run command: \npip install PyMySQL"
+            )
+
+        if not host:
+            host = os.getenv("HOST")
+
+        if not host:
+            raise ImproperlyConfigured("Please set your MySQL host")
+
+        if not dbname:
+            dbname = os.getenv("DATABASE")
+
+        if not dbname:
+            raise ImproperlyConfigured("Please set your MySQL database")
+
+        if not user:
+            user = os.getenv("USER")
+
+        if not user:
+            raise ImproperlyConfigured("Please set your MySQL user")
+
+        if not password:
+            password = os.getenv("PASSWORD")
+
+        if not password:
+            raise ImproperlyConfigured("Please set your MySQL password")
+
+        if not port:
+            port = os.getenv("PORT")
+
+        if not port:
+            raise ImproperlyConfigured("Please set your MySQL port")
+
+        conn = None
+
+        try:
+            conn = pymysql.connect(host=host,
+                                   user=user,
+                                   password=password,
+                                   database=dbname,
+                                   cursorclass=pymysql.cursors.DictCursor)
+        except pymysql.Error as e:
+            raise ValidationError(e)
+
+        def run_sql_mysql(sql: str) -> Union[pd.DataFrame, None]:
+            if conn:
+                try:
+                    cs = conn.cursor()
+                    cs.execute(sql)
+                    results = cs.fetchall()
+
+                    # Create a pandas dataframe from the results
+                    df = pd.DataFrame(
+                        results, columns=[desc[0] for desc in cs.description]
+                    )
+                    return df
+
+                except pymysql.Error as e:
+                    conn.rollback()
+                    raise ValidationError(e)
+
+                except Exception as e:
+                    conn.rollback()
+                    raise e
+
+        self.run_sql_is_set = True
+        self.run_sql = run_sql_mysql
+
+
     def connect_to_bigquery(self, cred_file_path: str = None, project_id: str = None):
         """
         Connect to gcs using the bigquery connector. This is just a helper function to set [`vn.run_sql`][vanna.base.base.VannaBase.run_sql]
