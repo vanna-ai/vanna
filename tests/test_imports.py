188bfc047ecff05000fff4af8bcf7c7f8c909264
André Pedersen <andrped94@gmail.com>
2024-03-30 11:08:14 +0100

    Added shortcut import tests

---
+++ tests/test_imports.py
@@ -0,0 +1,11 @@
+from vanna.anthropic import Anthropic_Chat
+from vanna.chromadb import ChromaDB_VectorStore
+from vanna.marqo import Marqo_VectorStore
+from vanna.mistral import Mistral
+from vanna.ollama import Ollama
+from vanna.openai import OpenAI_Chat, OpenAI_Embeddings
+from vanna.remote import VannaDefault
+from vanna.vannadb import VannaDB_VectorStore
+
+#from vanna.ZhipuAI import ZhipuAI_Chat
+#from vanna.ZhipuAI import ZhipuAI_Embeddings
