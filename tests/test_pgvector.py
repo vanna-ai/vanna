3aafae16a011894ee7dc8917853c3a2ad9ce2537
André Pedersen <andrped94@gmail.com>
2024-09-18 15:35:02 +0200

    Updated test script

--- tests/test_pgvector.py
+++ tests/test_pgvector.py
@@ -1,6 +1,26 @@
+import os
+
+from dotenv import load_dotenv
+
 from vanna.pgvector import PG_VectorStore
+
+load_dotenv()
+
+
+def get_vanna_connection_string():
+    server = os.environ.get("PG_SERVER")
+    driver = "psycopg"
+    port = 5432
+    database = os.environ.get("PG_DATABASE")
+    username = os.environ.get("PG_USERNAME")
+    password = os.environ.get("PG_PASSWORD")
+
+    return f"postgresql+psycopg://{username}:{password}@{server}:{port}/{database}"
 
 
 def test_pgvector():
-    pgclient = PG_VectorStore(config={"connection_string": ""})
+    connection_string = get_vanna_connection_string()
+    print("Connection string:")
+    print(connection_string)
+    pgclient = PG_VectorStore(config={"connection_string": connection_string})
     assert pgclient is not None
