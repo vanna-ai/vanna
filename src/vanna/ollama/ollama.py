fb5524c3b767e714d2a344be813a027b296fb18c
Mohammad Muzammil Shahid <muzammil.shahid@venturedive.com>
2025-07-08 12:34:05 +0500

    change: add support to turn off/on thinking in ollama models

--- src/vanna/ollama/ollama.py
+++ src/vanna/ollama/ollama.py
@@ -33,6 +33,7 @@ class Ollama(VannaBase):
     self.keep_alive = config.get('keep_alive', None)
     self.ollama_options = config.get('options', {})
     self.num_ctx = self.ollama_options.get('num_ctx', 2048)
+    self.think = config.get('think', True)
     self.__pull_model_if_ne(self.ollama_client, self.model)
 
   @staticmethod
@@ -96,7 +97,8 @@ class Ollama(VannaBase):
                                             messages=prompt,
                                             stream=False,
                                             options=self.ollama_options,
-                                            keep_alive=self.keep_alive)
+                                            keep_alive=self.keep_alive,
+                                            think=self.think)
 
     self.log(f"Ollama Response:\n{str(response_dict)}")
 
